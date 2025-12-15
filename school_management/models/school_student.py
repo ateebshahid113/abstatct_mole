4a6e50c4a08b3e91017d975ab75a3a987c2b8723
ateebshahid113 <ateebshahid113@gmail.com>
2025-12-15 15:10:09 +0500

    [Add]

--- school_management/models/school_student.py
+++ school_management/models/school_student.py
@@ -6,9 +6,8 @@ class SchoolStudent(models.Model):
     _description = 'School Student'
 
     age = fields.Integer(string='Age')
+    grade = fields.Char(string='Grade')
     room_id = fields.Many2one('school.room', string='Room')
-
-
 
     def get_student_info(self):
         """Get formatted student information"""
