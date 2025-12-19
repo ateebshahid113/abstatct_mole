f0591529dad383b858de34180139f4c64bb9afb7
ateebshahid113 <ateebshahid113@gmail.com>
2025-12-19 12:26:28 +0500

    Add text

--- school_management/__manifest__.py
+++ school_management/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'School Management',
+    'name': 'School Management System',
     'version': '18.0.1.0.0',
     'category': 'Education',
     'summary': 'Manage students, teachers and rooms',
